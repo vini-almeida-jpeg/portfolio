2ab66faadfe986787c2846cc47e46908b8be1f6c
vi-almeida-design <vi.almeidadesign@gmail.com>
2024-08-11 19:15:15 -0300

    Massive Development on the Home Page

---
+++ scripts/home.js
@@ -0,0 +1,87 @@
+//LP/Email Swiperr//
+var swiper = new Swiper(".lp-swiper", {
+    slidesPerView: 2,
+    spaceBetween: 15, 
+    freeMode:true,
+    grabCursor: 'true',
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1, freeMode:'true', 
+        },
+
+        780: {spaceBetween:15, slidesPerView: 2, freeMode:'true', 
+        },
+
+    },
+
+});
+
+//Ebook Swiper//
+
+var swiper = new Swiper(".ebooks-swiper", {
+    slidesPerView: 3,
+    spaceBetween: 15, 
+    freeMode:true,
+    grabCursor: 'true',
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1, freeMode:'true', 
+        },
+
+        780: {slidesPerView: 1, freeMode:'true', 
+        },
+
+        960: {slidesPerView:2, freeMode:'true',
+        },
+    },
+
+});
+
+
+
+
+//KV Ads Swiperr//
+
+var swiper = new Swiper(".kv-ads-swiper", {
+    slidesPerView: 3,
+    spaceBetween: 15, 
+    freeMode:true,
+    grabCursor: 'true',
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1, freeMode:'true', 
+        },
+
+        580: {spaceBetween:15, slidesPerView: 2, freeMode:'true', 
+        },
+
+        960: {slidesPerView:3, slidesPerGroup: 2, 
+        },
+    },
+
+});
+
+//KV Social Media Swiper//
+
+var swiper = new Swiper(".kv-sm-swiper", {
+    slidesPerView: 3,
+    spaceBetween: 15, 
+    freeMode:true,
+    grabCursor: 'true',
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1, freeMode:'true', 
+        },
+
+        580: {spaceBetween:15, slidesPerView: 2, freeMode:'true', 
+        },
+
+        960: {slidesPerView:4, freeMode:'true',
+        },
+    },
+
+});
