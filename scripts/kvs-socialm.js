5af3af768af50476a588d6872e89efe80b7ed235
vi-almeida-design <vi.almeidadesign@gmail.com>
2024-08-16 19:29:34 -0300

    Minor changes to the header and adding more assets

---
+++ scripts/kvs-socialm.js
@@ -0,0 +1,21 @@
+//KV Social Media Swiper//
+
+var swiper = new Swiper(".kv-sm-swiper", {
+    slidesPerView: "auto",
+    spaceBetween: 15, 
+    freeMode:'true',
+    grabCursor: 'true',
+
+    //breakpoints:{
+        //0: {
+        //    slidesPerView: 1, spaceBetween:15,  freeMode:'true',  
+        //},
+
+        //580: {spaceBetween:15, slidesPerView: 2, freeMode:'true', 
+        //},
+
+       //960: {slidesPerView:"auto", spaceBetween: 15, 
+        //},
+    //},
+
+});
